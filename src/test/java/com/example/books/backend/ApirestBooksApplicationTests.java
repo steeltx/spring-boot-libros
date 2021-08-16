b3f4abbdb937cf4a6fd1f2201dfc752cd9d317d0
steeltx <realsteel.gnu@gmail.com>
2021-08-15 23:48:28 -0500

    Init

---
+++ src/test/java/com/example/books/backend/ApirestBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.books.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
