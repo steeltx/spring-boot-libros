6aef6349a18f36a5bc3d231b31fb770718c39c54
steeltx <realsteel.gnu@gmail.com>
2021-08-17 23:47:13 -0500

    Implementando log en CategoriaService

--- src/main/java/com/example/books/backend/response/CategoriaResponseRest.java
+++ src/main/java/com/example/books/backend/response/CategoriaResponseRest.java
@@ -2,13 +2,13 @@ package com.example.books.backend.response;
 
 public class CategoriaResponseRest extends ResponseRest {
 
-	private CategoriaResponseRest categoriaResponse = new CategoriaResponseRest();
+	private CategoriaResponse categoriaResponse = new CategoriaResponse();
 
-	public CategoriaResponseRest getCategoriaResponse() {
+	public CategoriaResponse getCategoriaResponse() {
 		return categoriaResponse;
 	}
 
-	public void setCategoriaResponse(CategoriaResponseRest categoriaResponse) {
+	public void setCategoriaResponse(CategoriaResponse categoriaResponse) {
 		this.categoriaResponse = categoriaResponse;
 	}
 
--- src/main/java/com/example/books/backend/service/CategoriaServiceImpl.java
+++ src/main/java/com/example/books/backend/service/CategoriaServiceImpl.java
@@ -1,16 +1,44 @@
 package com.example.books.backend.service;
 
-import org.springframework.stereotype.Service;
+import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.books.backend.model.Categoria;
+import com.example.books.backend.model.dao.ICategoriaDao;
 import com.example.books.backend.response.CategoriaResponseRest;
 
 @Service
-public class CategoriaServiceImpl implements ICategoriaService{
+public class CategoriaServiceImpl implements ICategoriaService {
+
+	private static final Logger log = LoggerFactory.getLogger(CategoriaServiceImpl.class);
+
+	@Autowired
+	private ICategoriaDao categoriaDao;
 
 	@Override
+	@Transactional(readOnly = true)
 	public CategoriaResponseRest buscarCategorias() {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("Inicio de metodo buscarCategorias()");
+		CategoriaResponseRest response = new CategoriaResponseRest();
+
+		try {
+			List<Categoria> categoria = (List<Categoria>) categoriaDao.findAll();
+
+			response.getCategoriaResponse().setCategoria(categoria);
+
+			response.setMetadata("Respuesta ok", "200", "Respuesta exitosa");
+
+		} catch (Exception e) {
+			response.setMetadata("Respuesta no ok", "500", "Respuesta incorrecta");
+			log.error("Error al consultar categorias : " + e.getMessage());
+			e.getStackTrace();
+		}
+		return response;
 	}
-	
+
 }
