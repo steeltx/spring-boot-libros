73b9a4c743d5ef87a0341de66d25c9755d50ee38
steeltx <realsteel.gnu@gmail.com>
2021-08-17 23:31:18 -0500

    Clase service de categoria

---
+++ src/main/java/com/example/books/backend/service/CategoriaServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.books.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.books.backend.response.CategoriaResponseRest;
+
+@Service
+public class CategoriaServiceImpl implements ICategoriaService{
+
+	@Override
+	public CategoriaResponseRest buscarCategorias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/example/books/backend/service/ICategoriaService.java
@@ -0,0 +1,9 @@
+package com.example.books.backend.service;
+
+import com.example.books.backend.response.CategoriaResponseRest;
+
+public interface ICategoriaService {
+	
+	public CategoriaResponseRest buscarCategorias();
+
+}
